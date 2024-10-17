9e29fabfbc08e97052d49c5d351aad36b54e14ef
reetam100 <reetam.chatterjee@fortmindz.in>
2024-10-17 18:16:27 +0530

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,42 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(shop)"
+        options={{
+          headerShown: true,
+          title: "Shop",
+        }}
+      />
+      <Stack.Screen
+        name="categories"
+        options={{
+          headerShown: true,
+          title: "Categories",
+        }}
+      />
+      <Stack.Screen
+        name="product"
+        options={{
+          headerShown: true,
+          title: "Product",
+        }}
+      />
+      <Stack.Screen
+        name="auth"
+        options={{
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="cart"
+        options={{
+          presentation: "modal",
+          title: "Shopping Cart",
+        }}
+      />
+    </Stack>
+  );
+}
